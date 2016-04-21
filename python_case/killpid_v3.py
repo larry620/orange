42cd02c87b8b21db76bc253d1e6309f9bd4c8d5c
larry <larry620@sohu.com>
2016-04-22 03:04:32 +0800

    Fri Apr 22 03:04:32 CST 2016 from orange cl1.com

--- python_case/killpid_v3.py
+++ python_case/killpid_v3.py
@@ -1,18 +1,31 @@
+import commands
+import sys
+import os
+import linecache
+
 li = []
+
 def getPid(process):
-	import commands
-	import sys
-	import os
-#	import logging
 	cmd = "ps aux | grep -v grep |grep '%s' " % process
 	info = commands.getoutput(cmd)
-	txt = info.readlines()
-	print txt
+#	txt = info.readlines()
+#	print txt
 	infos = info.split('\n')
+	print "infos =", infos
+	aaa = [row[0] for row in info]
+	print "aaa =", aaa
 	for i in infos:
+		print "i =", i
 		j = i.split()
+			
+		print "j =", j
 		li.append(j[1])
 
 getPid('nginx')
-#for i in li:
-#    print i
+
+#if __name__ == '__main__':
+#	for b in li:
+#		cmd1 = "kill %s" % b
+#		end = commands.getoutput(cmd1)
+#		print cmd1
+#    
